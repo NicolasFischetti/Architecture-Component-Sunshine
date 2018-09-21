c966321b3729c6620594a34243bbe9951c9df5f7
Nicolas Fischetti <nicolasf@lagash.com>
2018-09-21 09:48:41 -0300

    Sunshine architecture

---
+++ architect/app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="b159801b-9465-49b9-b853-909a3783a56e")
+public class DataBindingInfo {}
